73866b44b61b932e8e9548b291347f993b7879c0
YashPatil2003 <102377635+YashPatil2003@users.noreply.github.com>
2023-11-15 14:26:22 +0530

    Add files via upload

---
+++ multiplier1.py
@@ -0,0 +1,173 @@
+def AND(A,B):
+    A=int(A)
+    B=int(B)
+    return A&B
+def half_adder(A, B):
+    A=int(A)
+    B=int(B)
+    sum_bit = A ^ B  # XOR operation
+    carry = A & B    # AND operation
+    return [sum_bit, carry]
+def full_adder(A,B,C):
+    A=int(A)
+    B=int(B)
+    sum = A^B^C
+    carry = (A&B) | (B&C) | (A&C)
+    return [sum,carry]
+#############################ACFGI############################
+def ACFGI_4(A,B,C,D):
+    A=int(A)
+    B=int(B)
+    C=int(C)
+    D=int(D)
+    sum = 1
+    carry = D
+    return [sum,carry]
+def ACFGI_3(A,B,C,D):
+    A=int(A)
+    B=int(B)
+    C=int(C)
+    D=int(D)
+    sum = 1
+    carry = C
+    return [sum,carry]
+def ACFGI_2(A,B,C,D):
+    A=int(A)
+    B=int(B)
+    C=int(C)
+    D=int(D)
+    sum = 1
+    carry = B
+    return [sum,carry]
+
+######################################AC6G#####################
+
+def AC6G_12(A,B,C,D):
+    A=int(A)
+    B=int(B)
+    C=int(C)
+    D=int(D)
+    sum = ((A|C)|(B|D))
+    carry = ((C&(B|D))|(A&D))
+    return [sum,carry]
+def AC6G_14(A,B,C,D):
+    A=int(A)
+    B=int(B)
+    C=int(C)
+    D=int(D)
+    sum = ((A|D)|(B|C))
+    carry = ((A&(B|C))|(C&D))
+    return [sum,carry]
+def AC6G_7(A,B,C,D):
+    A=int(A)
+    B=int(B)
+    C=int(C)
+    D=int(D)
+    sum = ((A|B)|(C|D))
+    carry = ((C&(A|B))|(A&B))
+    return [sum,carry]
+
+
+###############################MULTIPLIER##################
+def multiply1(A,B):
+    A=bin(A)[2:]
+    B=bin(B)[2:]
+    A=A.zfill(8)
+    B=B.zfill(8)
+    L=[]
+    N=len(A)
+    M=len(B)
+    
+    for i in range(M):
+        k = []
+        for j in range(N):
+            k.append(AND(A[j],B[M-1-i]))
+        L.append(k)
+    ##column 5 
+    (x19,y19) = ACFGI_4(L[0][3],L[1][4],L[2][5],L[3][6])
+
+    ##column 6
+    (x20,y20) =ACFGI_4(L[0][2],L[1][3],L[2][4],L[3][5])
+    (x21,y21) =ACFGI_4(L[4][6],L[5][7],x20,y19)
+
+    ##column 7
+    (x1,y1) = ACFGI_4(L[0][1],L[1][2],L[2][3],L[3][4])
+    (x2,y2) = ACFGI_4(L[4][5],L[5][6],L[6][7],0)
+    (x3,y3) = ACFGI_4(x1,x2,y20,0)
+    (m6,c6) = half_adder(1,y21)
+
+    ##column 8
+    (x4,y4) = ACFGI_4(L[0][0],L[1][1],L[2][2],L[3][3])    
+    (x5,y5) = ACFGI_4(L[4][4],L[5][5],L[6][6],L[7][7])
+    (x6,y6) = ACFGI_4(x4,x5,y1,y2)
+    (m7,c7) = full_adder(1,y3,c6)
+
+    ##column 9
+    (x7,y7) = ACFGI_4(L[1][0],L[2][1],L[3][2],L[4][3])  
+    (x8,y8) = ACFGI_4(L[5][4],L[6][5],L[7][6],0)
+    (x9,y9) = ACFGI_4(x7,x8,y4,y5)
+    (m8,c8) = full_adder(1,y6,c7)
+
+    ##column 10
+    (x10,y10) = ACFGI_2(L[2][0],L[3][1],L[4][2],L[5][3])
+    (x11,y11) = ACFGI_2(L[6][4],L[7][5],0,0)   
+    (x12,y12) = ACFGI_3(x10,x11,y7,y8)
+    (m9,c9)   = full_adder(1,y9,c8)
+    
+    ##column 11
+    (x13,y13) = AC6G_12(L[3][0],L[4][1],L[5][2],L[6][3])
+    (x14,y14) = AC6G_7(L[7][4],x13,y10,y11)
+    (m10,c10) = full_adder(x14,y12,c9)
+
+    ##column 12 
+    (x15,y15) = AC6G_14(L[4][0],L[5][1],L[6][2],L[7][3])
+    (x16,y16) = half_adder(x15,y13)
+    (m11,c11) = full_adder(x16,y14,c10)
+
+    ##column 13
+    (x17,y17) = AC6G_7(L[5][0],L[6][1],L[7][2],y15)
+    (m12,c12) = full_adder(x17,y16,c11)
+
+    ##column 14
+    (x18,y18) = half_adder(L[6][0],L[7][1])
+    (m13,c13) = full_adder(x18,y17,c12)
+
+    ##column 15 
+    (m14,c14) = full_adder(L[7][0],c13,y18)
+
+    m15 = c14
+    m0 = m1 = m2 = m3 = 0
+    m4 = m5 = 1 
+    out=[m15,m14,m13,m12,m11,m10,m9,m8,m7,m6,m5,m4,m3,m2,m1,m0]
+    result = ''.join(map(str, out))
+    return(int(result,2))
+########################################################################################
+##### ER NMED AND MRED CALCULATION #####################################################
+
+t = 256
+k = m = x = p = 0
+for i in range(1,t):
+    for j in range(1,t):
+        temp = abs(multiply1(i,j)-i*j)
+        k+= temp
+        m+=temp/(i*j)
+        if(temp!=0):
+            x=x+1
+        else:
+            x=x
+
+MED = (k)/(t*t)
+MRED = m/(t*t)
+NMED = k/((t*t)*(t-1)*(t-1))
+ER =(x/(256*256))*100
+print("NMED:" + str(NMED))
+print("MRED:" +str(MRED))
+print("ER:" + str(ER))
+
+
+print(250*250)
+print(multiply1(250,250))
+print(200*200)
+print(multiply1(200,200))
+print(190*190)
+print(multiply1(5,240))
